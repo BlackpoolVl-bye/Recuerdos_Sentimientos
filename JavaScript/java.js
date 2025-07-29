2128c40e315bb73b861db646b834d5fd3f51edcf
BlackpoolVl-bye <davidmartinez24242424@gmail.com>
2025-07-28 22:21:20 -0400

    MEGA ACTUALIZACION 7.5

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,14 +1,3 @@
 const audio = document.getElementById('audio');
 const playButton = document.getElementById('playButton');
 
-/*
-playButton.addEventListener('click', () => {
-  if (audio.paused) {
-    audio.play();
-    playButton.textContent = 'Pausar';
-  } else {
-    audio.pause();
-    playButton.textContent = 'Reproducir';
-  }
-});
-*/
